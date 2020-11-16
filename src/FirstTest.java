4d832c63b4f1b3d3913f33313f52ded0018e184a
Tetiana Yeremeieva <tetiana.yeremeieva@betlab.com>
2020-11-16 20:54:00 +0200

    Lecture 3

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,9 +1,14 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
@@ -31,9 +36,160 @@ public class FirstTest {
         driver.quit();
     }
 
+
     @Test
-    public void FirstTest()
+    public void firstTest()
     {
-        System.out.println("First test run");
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'SKIP')]"),
+                "Cannot find Skip button",
+                1);
+        waitForElementAndClick(
+                By.xpath("//*[contains(@resource-id,'search_container')]"),
+                "Cannot find search input",
+                3);
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@resource-id,'search_container')]"),
+                "Cannot find search input",
+                5,
+                "Java"
+        );
+        waitForElementPresent(
+                By.xpath("//*[@text='Java (programming language)']"),
+                "Cannot find 'Java (programming language)' topic searching by 'Java'",
+                15
+        );
+
+
     }
+
+
+    @Test
+    public void testCancelSearch(){
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/fragment_onboarding_skip_button"),
+                "Cannot find 'Skip' button",
+                3
+        );
+
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/search_container"),
+                "Cannot find 'Search' field",
+                3
+        );
+
+        waitForElementAndSendKeys(
+                By.id("org.wikipedia:id/search_container"),
+                "Cannot find 'Search' field",
+                10,
+                "Java"
+        );
+
+        waitForElementAndClear(
+                By.id("org.wikipedia:id/search_container"),
+                "Cannot find 'Search' field",
+                10
+        );
+
+//        waitForElementAndClick(
+//                By.id("org.wikipedia:id/search_close_btn"),
+//                "Cannot find close icon",
+//                5
+//        );
+
+        waitForElementNotPresent(
+                By.id("org.wikipedia:id/search_close_btn"),
+                "X is still present on the page",
+                5
+        );
+
+    }
+
+
+    @Test
+    public void testCompareArticleTitle()
+    {
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/fragment_onboarding_skip_button"),
+                "Cannot find 'Skip' button",
+                3
+        );
+
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/search_container"),
+                "Cannot find 'Search' field",
+                3
+        );
+
+        waitForElementAndSendKeys(
+                By.id("org.wikipedia:id/search_container"),
+                "Cannot find 'Search' field",
+                10,
+                "Java"
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@text='Java (programming language)']"),
+                "Cannot find 'Java (programming language)' topic searching by 'Java'",
+                15
+        );
+
+        WebElement title_element = waitForElementPresent(
+                By.xpath("//*[@content-desc=\"Java (programming language)\"]"),
+                "Cannot find article subtitle",
+                15
+        );
+
+        String article_title = title_element.getAttribute("name");
+        Assert.assertEquals(
+                "We see unexpected title",
+                "Java (programming language)",
+                article_title
+        );
+
+
+    }
+
+    private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds)
+    {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(error_message + "\n");
+        return wait.until(
+                ExpectedConditions.presenceOfElementLocated(by)
+        );
+
+    }
+
+    private WebElement waitForElementAndClick(By by, String errr_message, long timeoutInSeconds){
+        WebElement element = waitForElementPresent(by, errr_message, timeoutInSeconds);
+        element.click();
+        return element;
+
+    }
+
+
+    private WebElement waitForElementAndSendKeys(By by, String errr_message, long timeoutInSeconds, String value){
+        WebElement element = waitForElementPresent(by, errr_message, timeoutInSeconds);
+        element.sendKeys(value);
+        return element;
+
+    }
+
+
+    private boolean waitForElementNotPresent(By by, String error_message, long timeoutInSeconds )
+    {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(error_message);
+        return wait.until(
+                ExpectedConditions.invisibilityOfElementLocated(by)
+        );
+    }
+
+    private WebElement waitForElementAndClear(By by, String error_message, long timeoutInSeconds)
+    {
+        WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
+        element.clear();
+        return element;
+    }
+
 }
